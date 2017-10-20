56c14cf2313e54c7f36eadc20fde75ac1eb21a7c
shy <shyamal.punekar@gmail.com>
2017-10-20 16:25:28 -0700

    Add DoctorPagerAdapter to DoctorDetailActivity

--- app/src/main/java/com/epicodus/doctorlookup/adapters/DoctorPagerAdapter.java
+++ app/src/main/java/com/epicodus/doctorlookup/adapters/DoctorPagerAdapter.java
@@ -1,8 +1,41 @@
 package com.epicodus.doctorlookup.adapters;
 
+import android.support.v4.app.FragmentPagerAdapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.epicodus.doctorlookup.models.Doctor;
+import com.epicodus.doctorlookup.ui.DoctorDetailFragment;
+
+import java.util.ArrayList;
+
+
 /**
  * Created by spuek on 10/20/17.
  */
 
-public class DoctorPagerAdapter {
+public class DoctorPagerAdapter extends FragmentPagerAdapter{
+    private ArrayList<Doctor> mDoctors;
+
+    public DoctorPagerAdapter(FragmentManager fm, ArrayList<Doctor> doctors) {
+        super(fm);
+        mDoctors = doctors;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return DoctorDetailFragment.newInstance(mDoctors.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mDoctors.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mDoctors.get(position).getFirstName();
+    }
 }
--- app/src/main/java/com/epicodus/doctorlookup/ui/DoctorDetailActivity.java
+++ app/src/main/java/com/epicodus/doctorlookup/ui/DoctorDetailActivity.java
@@ -1,15 +1,37 @@
 package com.epicodus.doctorlookup.ui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.epicodus.doctorlookup.R;
+import com.epicodus.doctorlookup.adapters.DoctorPagerAdapter;
+import com.epicodus.doctorlookup.models.Doctor;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class DoctorDetailActivity extends AppCompatActivity {
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
+    private DoctorPagerAdapter adapterViewPager;
+    ArrayList<Doctor> mDoctors = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_detail);
+        ButterKnife.bind(this);
+
+        mDoctors = Parcels.unwrap(getIntent().getParcelableExtra("doctors"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new DoctorPagerAdapter(getSupportFragmentManager(), mDoctors);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
