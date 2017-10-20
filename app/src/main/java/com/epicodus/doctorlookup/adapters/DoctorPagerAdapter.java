15d57a3272ba4bb9142cf0a7736fd5b066a7ca48
shy <shyamal.punekar@gmail.com>
2017-10-20 16:02:28 -0700

    Add custom fragment and pageradapters

---
+++ app/src/main/java/com/epicodus/doctorlookup/adapters/DoctorPagerAdapter.java
@@ -0,0 +1,8 @@
+package com.epicodus.doctorlookup.adapters;
+
+/**
+ * Created by spuek on 10/20/17.
+ */
+
+public class DoctorPagerAdapter {
+}
--- app/src/main/java/com/epicodus/doctorlookup/ui/DoctorDetailFragment.java
+++ app/src/main/java/com/epicodus/doctorlookup/ui/DoctorDetailFragment.java
@@ -6,13 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.epicodus.doctorlookup.R;
+import com.epicodus.doctorlookup.models.Doctor;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DoctorDetailFragment extends Fragment {
+
+//    @Bind(R.id.doctorImageView)
+//    ImageView mImageLabel;
+    @Bind(R.id.doctorNameTextView) TextView mNameLabel;
+//    @Bind(R.id.cuisineTextView) TextView mCategoriesLabel;
+    @Bind(R.id.ratingTextView) TextView mRatingLabel;
+    @Bind(R.id.websiteTextView) TextView mWebsiteLabel;
+    @Bind(R.id.phoneTextView) TextView mPhoneLabel;
+    @Bind(R.id.addressTextView) TextView mAddressLabel;
+    @Bind(R.id.saveDoctorButton)
+    TextView mSaveRestaurantButton;
+
+    private Doctor mDoctor;
 
 
     public DoctorDetailFragment() {
