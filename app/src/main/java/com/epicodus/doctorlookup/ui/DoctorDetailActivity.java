edca15a7214adfbee898ce328df150f3838dc71b
shy <shyamal.punekar@gmail.com>
2017-10-20 15:24:12 -0700

    Add DoctorDetailActivity

---
+++ app/src/main/java/com/epicodus/doctorlookup/ui/DoctorDetailActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.doctorlookup.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.doctorlookup.R;
+
+public class DoctorDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_detail);
+    }
+}
