30f66395ea37545895c7b1df31da82e0eb0cf8df
shy <shyamal.punekar@gmail.com>
2017-10-28 12:50:20 -0700

    Add SavedDoctorListActivitys activity to display data

---
+++ app/src/main/java/com/epicodus/doctorlookup/ui/SavedDoctorListActivity.java
@@ -0,0 +1,12 @@
+package com.epicodus.doctorlookup.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedDoctorListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
