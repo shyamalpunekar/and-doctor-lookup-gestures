86c1a5c62fbebd20898f980fe35f288d0b07d377
shy <shyamal.punekar@gmail.com>
2017-10-28 18:29:19 -0700

    Add logout option

--- app/src/main/java/com/epicodus/doctorlookup/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/doctorlookup/ui/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 
 import com.epicodus.doctorlookup.Constants;
 import com.epicodus.doctorlookup.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -77,7 +81,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mFindDoctorsButton.setOnClickListener(this);
     }
 
-        @Override
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+
+    @Override
         public void onClick(View v) {
 
             if (v == mFindDoctorsButton) {
